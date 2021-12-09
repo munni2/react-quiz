5e58d74268031d9c1e6ccb4ec01b85e795955d87
munni <munnimodak@gmail.com>
2021-12-09 12:04:21 +0600

    Fixed router issue

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import image from "../assets/images/3.jpg";
-import classes from "../styles/video.module.css"
+import classes from "../styles/video.module.css";
 
 const Video = () => {
     return ( 
       
-        <a href="quiz.html">
+        <Link to="/quiz">
             <div className={classes.video}>
               <img src={image} alt="" />
               <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
@@ -13,7 +14,7 @@ const Video = () => {
                 <p>Score : Not taken yet</p>
               </div>
             </div>
-        </a>
+        </Link>
       
     );
 }
--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,9 +1,9 @@
+import {Link} from "react-router-dom";
 import classes from "../../styles/login.module.css";
 import Illustration from "../Illustration";
 import Form from "../Form";
 import TextInput from "../TextInput";
 import Button from "../Button";
-import {Link} from "react-router-dom";
 
 
 const Login = () => {
@@ -16,7 +16,7 @@ const Login = () => {
                 <TextInput type="text" placeholder="Enter email" icon="alternate_email" />
                 <TextInput type="password" placeholder="Enter password" icon="lock" />
                 <Button><span>Submit now</span></Button>
-                <div class="info">Don't have an account? <Link href="/signup">Signup</Link> instead.</div>
+                <div class="info">Don't have an account? <Link to="/signup">Signup</Link> instead.</div>
             </Form> 
         </div>
         </>
